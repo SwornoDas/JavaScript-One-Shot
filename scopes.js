f16df0636e2995e76c24622e086ece23182c2b9f
Sworno Das <95184683+SwornoDas@users.noreply.github.com>
2025-08-10 21:43:32 +0530

    Added scopes.js

---
+++ scopes.js
@@ -0,0 +1,30 @@
+//scopes of the program
+
+// var c = 30000
+
+let a = 300;
+if (true) {
+  let a = 10
+  const b = 20
+  //   c = 30;
+  //   var c = 30;
+  console.log("INNER: ", a);
+  function addnum(){
+    
+  }
+}
+
+// for (let i = 0; i < 5; i++) {
+//   let a = 100;
+//   const b = 200;
+//   //   c = 300;
+//   //   var c = 300;
+//   console.log("LOOP: ", a);
+// }
+
+
+
+console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(a, b, c);
