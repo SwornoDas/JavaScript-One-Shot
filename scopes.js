ae899359d7c59b34f8388f4d2220c3dfbedb69c2
Sworno Das <95184683+SwornoDas@users.noreply.github.com>
2025-08-10 22:04:33 +0530

    Updated Scopes.js file

--- scopes.js
+++ scopes.js
@@ -4,14 +4,12 @@
 
 let a = 300;
 if (true) {
-  let a = 10
-  const b = 20
+  let a = 10;
+  const b = 20;
   //   c = 30;
   //   var c = 30;
   console.log("INNER: ", a);
-  function addnum(){
-    
-  }
+  function addnum() {}
 }
 
 // for (let i = 0; i < 5; i++) {
@@ -22,9 +20,47 @@ if (true) {
 //   console.log("LOOP: ", a);
 // }
 
-
-
 console.log(a);
 // console.log(b);
 // console.log(c);
 // console.log(a, b, c);
+
+// ***********Nested scopes *********//
+
+function one() {
+  const username = "Sworno";
+
+  function two() {
+    const website = "YouTube";
+    console.log(username);
+  }
+  //console.log(website) // This will throw an error because 'website' is not defined in this scope
+  two();
+}
+// one();
+
+if (true) {
+  const username = "sworno's";
+  if (username === "sworno's") {
+    const website = " youtube channel";
+    // console.log(username + website);
+  }
+  // console.log(website)
+}
+// console.log(username); // This will throw an error because 'username' is not defined in this scope
+
+// +++++++++++++++++ Inssterestion ++++++++++++
+console.log(addone(5))
+function addone(num) {
+  return num + 1;
+}
+
+
+// console.log(addTwo(5)); 
+// This will throw an error because 'addTwo' is not defined yet
+const addTwo = function (num) {
+  return num + 2;
+};
+
+
+
