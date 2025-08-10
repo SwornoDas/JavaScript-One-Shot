f13b49b853e851e8d85dcb884c99713c67234b60
Sworno Das <95184683+SwornoDas@users.noreply.github.com>
2025-08-10 18:55:30 +0530

    first commit

---
+++ comparision.js
@@ -0,0 +1,31 @@
+console.log(2 > 1);
+console.log(2 < 1);
+console.log(2 >= 1);
+console.log(2 <= 1);
+console.log(2 == 1);
+console.log(2 != 1);
+console.log(2 === 1);
+console.log(2 !== 1);
+
+console.log("new line");
+console.log("2" > 1);
+console.log("02" > 1);
+console.log("2" < 1);
+console.log("02" < 1);
+console.log("2" >= 1);
+console.log("02" >= 1);
+console.log("2" <= 1);
+console.log("02" <= 1);
+console.log("2" == 1);
+console.log("02" == 1);
+console.log("2" != 1);
+console.log("02" != 1);
+console.log("2" === 1);
+console.log("02" === 1);
+console.log("2" !== 1);
+console.log("02" !== 1);
+
+
+
+
+
