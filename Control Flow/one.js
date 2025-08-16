c49f4de4105633bbef7462d5ebd3dead38fdc76a
Sworno Das <95184683+SwornoDas@users.noreply.github.com>
2025-08-16 17:41:57 +0530

    Added one.js

---
+++ Control Flow/one.js
@@ -0,0 +1,67 @@
+// if
+if(true){
+    
+}
+if(false){
+    
+}
+
+const isUserLoggedIn = true;
+
+
+if (isUserLoggedIn){
+
+}
+2<=2
+3 != 2
+
+if (2 === "2"){
+    console.log("executed")
+}
+if (2 !== 3){
+    console.log("executed")
+}
+const temperature = 41
+if (temperature === 40){
+    console.log("less than 50")
+}   else{
+    console.log("Temperature is greater than or equal to 50")
+}
+console.log("Execute")
+// <, >, <=, >=, ==, !=, ===, !==,
+
+// const score = 300
+
+// if (score > 100){
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+// console.log(`User power: ${power}`);
+
+// const balance = 1000;
+// if (balance> 500) console.log("test"), console.log("test2");
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+// } else if (balance < 900) {
+//     console.log("less than 900");
+// } else {
+//     console.log("less than 1200");
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+// if (userLoggedIn && debitCard && 2==3) {
+//     console.log("Allow to buy course")
+// } else {
+//     console.log("User cannot make a purchase")
+// }
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User Logged In");
+} 
