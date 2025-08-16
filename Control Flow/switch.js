4fa94765c09e6101c45c9837e4c5004411f3032f
Sworno Das <95184683+SwornoDas@users.noreply.github.com>
2025-08-16 17:55:38 +0530

    Added switch.js

---
+++ Control Flow/switch.js
@@ -0,0 +1,41 @@
+// switch (Key) {
+//     case value:
+//         break;
+//     default:
+//         break;
+// }
+
+const fruit = "apple";
+switch (fruit) {
+  case "banana":
+    console.log("Yellow fruit");
+    break;
+  case "apple":
+    console.log("Red fruit");
+    break;
+  case "grape":
+    console.log("Purple fruit");
+    break;
+  default:
+    console.log("Unknown fruit");
+}
+
+const month = 3;
+
+switch (month) {
+  case 1:
+    console.log("January");
+    break;
+  case 2:
+    console.log("February");
+    break;
+  case 3:
+    console.log("March");
+    break;
+  case 4:
+    console.log("April");
+    break;
+  default:
+    console.log("Unknown month");
+    break;
+}
