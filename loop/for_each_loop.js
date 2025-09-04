d0c05be852310b039c018629e1932f2d425cacc5
Sworno Das <95184683+SwornoDas@users.noreply.github.com>
2025-09-05 00:12:38 +0530

    Added for_each_loop.js

---
+++ loop/for_each_loop.js
@@ -0,0 +1,8 @@
+const coding  = ["JS", "C++", "Ruby", "Python", "Swift"];
+coding.forEach( function (techitem){
+    console.log(techitem);
+})
+coding.forEach ( (item) => {
+    console.log(item);
+})
+
