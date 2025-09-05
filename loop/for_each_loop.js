1f68fed30ccb895c63e322ce2a177ac707dd35b4
Sworno Das <95184683+SwornoDas@users.noreply.github.com>
2025-09-05 11:25:42 +0530

    Updated for_each_loop.js file

--- loop/for_each_loop.js
+++ loop/for_each_loop.js
@@ -1,8 +1,49 @@
-const coding  = ["JS", "C++", "Ruby", "Python", "Swift"];
-coding.forEach( function (techitem){
-    console.log(techitem);
-})
-coding.forEach ( (item) => {
-    console.log(item);
-})
+const coding = ["JS", "C++", "Ruby", "Python", "Swift"];
+coding.forEach(function (techitem) {
+  console.log(techitem);
+});
+coding.forEach((item) => {
+  console.log(item);
+});
+
+console.log();
+
+function printMe(item) {
+  console.log(item);
+}
+coding.forEach(printMe);
+console.log();
+
+coding.forEach((item, index, arr) => {
+  console.log(index, item, arr);
+});
+
+console.log();
+const myCoding = [
+  {
+    LangName: "JavaScript",
+    LangFileName: "JS"
+  },
+  {
+    LangName: "C++",
+    LangFileName: "C++"
+  },
+  {
+    LangName: "Ruby",
+    LangFileName: "Ruby"
+  },
+  {
+    LangName: "Python",
+    LangFileName: "Python"
+  },
+  {
+    LangName: "Swift",
+    LangFileName: "Swift"
+  },
+];
+
+
+myCoding.forEach((item) => {
+  console.log(item.LangName, item.LangFileName);
+});
 
